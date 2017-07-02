4b4665a9537f23a16399b5fb9fa885a13cf7d64b
c-Nosetab <cpbateson@gmail.com>
2017-07-02 11:44:52 -0500

    added database

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  namespace :api do
+    namespace :api do
+      get '/' => 'students'
+      get '/students' => '/students#index'
+      post '/students' => '/studentsr#post'
+      get '/students/:id' => '/students#show'
+      get '/students/:id/edit' => '/students#edit'
+      patch '/students/:id' => '/students#update'
+      delete '/students/:id' => '/students#destroy'
+
+      get '/students/:user_id/experiences' => '/experiences#index'
+      post '/students/:user_id/experiences' => '/experiences#post'
+      get '/students/:user_id/experiences/:exp_id' => '/experiences#show'
+      get '/students/:user_id/experiences/:exp_id/edit' => '/experiences#edit'
+      patch '/students/:user_id/experiences/:exp_id' => '/experiences#update'
+      delete '/students/:user_id/experiences/:exp_id' => '/experiences#destroy'
+
+
+    end
+  end
+
+
 end

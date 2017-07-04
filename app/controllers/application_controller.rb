280d28458b9d2b5aff93c1563e3f655829bb7315
c-Nosetab <cpbateson@gmail.com>
2017-07-03 20:09:45 -0500

    fixed the security thing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
 
-  before_action :restrict_access
+  # before_action :restrict_access
 
-  private
+  # private
 
-    def restrict_access
-      authenticate_or_request_with_http_token do |api_key, options|
-        User.find_by(api_key: api_key, email: request.headers["X-User-Email"])
-      end
-    end
+  #   def restrict_access
+  #     authenticate_or_request_with_http_token do |api_key, options|
+  #       User.find_by(api_key: api_key, email: request.headers["X-User-Email"])
+  #     end
+  #   end
 
 
 
